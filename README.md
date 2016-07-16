7aaa5f1f4c0f570122b3a545ad63480d4b770633
Anthony Lauzon <anthony.lauzon@gmail.com>
2016-07-15 22:19:27 -0400

    Update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rmsnormalize
 Command line tool that limits an audio file using Waves L3 Ultramaximizer to a target RMS.
+
+## Requirements
+- sox
+- MrsWatson
+- hexdump
+- xxd
+- Waves L3 Ultramaximizer
+
+## Usage
+
+$ rmsnormalize.sh <INPUT_FILENAME> <TARGET_RMS>
--- rmsnormalize.sh
+++ rmsnormalize.sh
@@ -3,11 +3,45 @@
 filename="$1"
 target_rms="$2"
 
-rms=`sox "$filename" -n stats 2>&1 | grep "RMS lev dB" | awk '{print $4}'`
-threshold=`echo $target_rms $rms |  awk '{printf "%f",  - ($1 - $2)}'`
-threshold_hex=`echo $threshold | xxd  -p`
+ext="${filename##*.}"
+prefix="${filename%.*}"
 
-hexdump -ve '1/1 "%.2X"' L3.fxp.template | \
-  sed "s/5E/$threshold_hex/g" | xxd -r -p > $filename.fxp
+sample_rate=`soxi $filename | \
+  grep "Sample Rate" | \
+  awk '{print $4}'`
 
+precision=`soxi $filename | \
+  grep "Precision" | \
+  awk '{print $3}' | \
+  sed 's/-bit//g'`
 
+rms=`sox "$filename" -n stats 2>&1 | \
+  grep "RMS lev dB" | \
+  awk '{print $4}'`
+
+threshold=`echo $target_rms $rms | \
+  awk '{printf "%f",  - ($1 - $2)}' | \
+  xxd -p`
+
+hexdump -ve '1/1 "%.2X"' L3.fxb.template | \
+  sed "s/5E/$threshold/g" | \
+  xxd -r -p > $filename.fxb
+
+sox $filename \
+  --bits $precision \
+  --rate $sample_rate \
+  --encoding signed-integer \
+  --endian little \
+  $prefix.raw
+
+mrswatson64 \
+  --input $prefix.raw \
+  --output $prefix.norm.raw \
+  --plugin "WaveShell-VST 9.6:L3GS,$filename.fxb"
+
+sox \
+  --bits $precision \
+  --rate $sample_rate \
+  --encoding signed-integer \
+  --endian little \
+  $prefix.norm.raw $prefix.norm.$ext
